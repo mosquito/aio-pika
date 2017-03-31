dd70de5def30edb147b03e7c3bcfaad691f9253e
Dmitry Orlov <me@mosquito.su>
2017-03-31 16:56:02 +0300

    bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info < (3, 4):
 
 setup(
     name='aio-pika',
-    version='0.6.3',
+    version='0.6.4',
     author="Dmitry Orlov <me@mosquito.su>",
     author_email="me@mosquito.su",
     license="Apache Software License",
