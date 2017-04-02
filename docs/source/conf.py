59a8e21db8f875b30a381c3697aa396bb62fb5c3
Dmitry Orlov <me@mosquito.su>
2017-04-03 02:43:30 +0300

    add some doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,10 +36,12 @@ apidoc_main(['-f', '--no-toc', '-o', 'apidoc', '../../aio_pika/'])
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
