4477e28c29404a04fc8fa33921943b0c0d474f7b
Dmitry Orlov <me@mosquito.su>
2017-04-21 00:20:34 +0300

    add doc for part 3

---
+++ docs/source/rabbitmq-tutorial/examples/3-publish-subscribe/emit_log.py
@@ -0,0 +1,32 @@
+import sys
+import asyncio
+from aio_pika import connect, Message, DeliveryMode, ExchangeType
+
+
+async def main(loop):
+    # Perform connection
+    connection = await connect("amqp://guest:guest@localhost/", loop=loop)
+
+    # Creating a channel
+    channel = await connection.channel()
+
+    logs_exchange = await channel.declare_exchange('logs', ExchangeType.FANOUT)
+
+    message_body = b' '.join(sys.argv[1:]) or b"Hello World!"
+
+    message = Message(
+        message_body,
+        delivery_mode=DeliveryMode.PERSISTENT
+    )
+
+    # Sending the message
+    await logs_exchange.publish(message, routing_key='task_queue')
+
+    print(" [x] Sent %r" % message)
+
+    await connection.close()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
---
+++ docs/source/rabbitmq-tutorial/examples/3-publish-subscribe/receive_logs.py
@@ -0,0 +1,40 @@
+import asyncio
+from aio_pika import connect, IncomingMessage, ExchangeType
+
+loop = asyncio.get_event_loop()
+
+
+def on_message(message: IncomingMessage):
+    print("[x] %r" % message.body)
+
+
+async def main():
+    # Perform connection
+    connection = await connect("amqp://guest:guest@localhost/", loop=loop)
+
+    # Creating a channel
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=1)
+
+    logs_exchange = await channel.declare_exchange(
+        'logs',
+        ExchangeType.FANOUT
+    )
+
+    # Declaring queue
+    queue = await channel.declare_queue(exclusive=True)
+
+    # Binding the queue to the exchange
+    await queue.bind(logs_exchange)
+
+    # Start listening the queue with name 'task_queue'
+    await queue.consume(on_message)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+
+    # we enter a never-ending loop that waits for data and runs callbacks whenever necessary.
+    print(' [*] Waiting for logs. To exit press CTRL+C')
+    loop.run_forever()
