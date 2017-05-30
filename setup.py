9f0b9932fe9636912301a5129c47e2f83f4d96dd
Dmitry Orlov <me@mosquito.su>
2017-05-30 14:05:24 +0300

    [fix] freeze pytest-asyncio version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
             'coveralls',
             'pylama',
             'pytest',
-            'pytest-asyncio',
+            'pytest-asyncio<0.6',
             'pytest-cov',
             'sphinx',
             'sphinx-autobuild',
