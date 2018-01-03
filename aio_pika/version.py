689765ede490159d4ce0795147993e3ce9cdba40
Dmitry Orlov <orlov@edadeal.ru>
2018-01-03 12:43:02 +0300

    [fix] bump

--- aio_pika/version.py
+++ aio_pika/version.py
@@ -7,7 +7,7 @@ package_license = "Apache Software License"
 
 team_email = 'me@mosquito.su'
 
-version_info = (1, 7, 2)
+version_info = (1, 7, 3)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
