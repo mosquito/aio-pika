98945f60d8e3e88a53fe3d034baec1fcb4fa40cb
Dmitry Orlov <me@mosquito.su>
2018-02-20 13:52:58 +0300

    [fix] transactions and documentation

--- docs/source/examples/simple_publisher_transactions.py
+++ docs/source/examples/simple_publisher_transactions.py
@@ -9,14 +9,49 @@ async def main(loop):
     async with connection:
         routing_key = "test_queue"
 
-        channel = await connection.channel()
+        # Transactions conflicts with `publisher_confirms`
+        channel = await connection.channel(publisher_confirms=False)
+
+        # Use transactions with async context manager
+        async with channel.transaction():
+            # Publishing messages but delivery will not be done
+            # before committing this transaction
+            for i in range(10):
+                message = aio_pika.Message(
+                    body='Hello #{}'.format(i).encode()
+                )
+
+                await channel.default_exchange.publish(
+                    message, routing_key=routing_key
+                )
+
+        # Using transactions manually
+        tx = channel.transaction()
+
+        # start transaction manually
+        await tx.select()
 
         await channel.default_exchange.publish(
-            aio_pika.Message(
-                body='Hello {}'.format(routing_key).encode()
-            ),
+            aio_pika.Message(body='Hello {}'.format(routing_key).encode()),
             routing_key=routing_key
         )
+
+        await tx.commit()
+        tx.close()
+
+        # Using transactions manually
+        tx = channel.transaction()
+
+        # start transaction manually
+        await tx.select()
+
+        await channel.default_exchange.publish(
+            aio_pika.Message(body='Should be rejected'.encode()),
+            routing_key=routing_key
+        )
+
+        await tx.rollback()
+        tx.close()
 
 
 if __name__ == "__main__":
