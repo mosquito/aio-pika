1e55d1acc2753120157cab0a148701894e4992a1
Michal Jagiello <michal.jagiello@nask.pl>
2018-03-07 08:55:23 +0100

    Use ExternalCredentials when no login provided

---
+++ docs/source/examples/external-credentials.py
@@ -0,0 +1,33 @@
+import asyncio
+import aio_pika
+import ssl
+
+async def main(loop):
+    connection = await aio_pika.connect_robust(host='127.0.0.1',
+        login='',
+        ssl=True,
+        ssl_options=dict(
+            ca_certs="cacert.pem",
+            certfile="cert.pem",
+            keyfile="key.pem",
+            cert_reqs=ssl.CERT_REQUIRED,
+        ), loop=loop
+    )
+
+    async with connection:
+        routing_key = "test_queue"
+
+        channel = await connection.channel()
+
+        await channel.default_exchange.publish(
+            aio_pika.Message(
+                body='Hello {}'.format(routing_key).encode()
+            ),
+            routing_key=routing_key
+        )
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+    loop.close()
