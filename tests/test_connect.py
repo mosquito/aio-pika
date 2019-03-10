18d5c7618ec2ad967bfb2bc4b7979dc55f73e3f7
Dmitry Orlov <me@mosquito.su>
2019-03-10 22:40:25 +0300

    add connect tests

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,44 +1,47 @@
+import asyncio
+
 from yarl import URL
 
 from aio_pika import connect
-from tests import BaseTestCase
+import pytest
 
 
-class TestConnectFunction(BaseTestCase):
-    class FakeConnection:
-        def __init__(self, url, **kwargs):
-            self.url = URL(url)
-            self.kwargs = kwargs
+VARIANTS = (
+    (dict(url="amqp://localhost/"), "amqp://localhost/"),
+    (dict(url="amqp://localhost"), "amqp://localhost/"),
+    (dict(url="amqp://localhost:5674"), "amqp://localhost:5674/"),
+    (dict(url="amqp://localhost:5674//"), "amqp://localhost:5674//"),
+    (dict(url="amqp://localhost:5674/"), "amqp://localhost:5674/"),
+    (
+        dict(host="localhost", port=8888),
+        "amqp://guest:guest@localhost:8888//"
+    ),
+    (
+        dict(host="localhost", port=8888, virtualhost="foo"),
+        "amqp://guest:guest@localhost:8888/foo"
+    ),
+    (
+        dict(host="localhost", port=8888, virtualhost="/foo"),
+        "amqp://guest:guest@localhost:8888//foo"
+    ),
+)
 
-        async def connect(self):
-            return
 
-    VARIANTS = (
-        (dict(url="amqp://localhost/"), "amqp://localhost/"),
-        (dict(url="amqp://localhost"), "amqp://localhost/"),
-        (dict(url="amqp://localhost:5674"), "amqp://localhost:5674/"),
-        (dict(url="amqp://localhost:5674//"), "amqp://localhost:5674//"),
-        (dict(url="amqp://localhost:5674/"), "amqp://localhost:5674/"),
-        (
-            dict(host="localhost", port=8888),
-            "amqp://guest:guest@localhost:8888//"
-        ),
-        (
-            dict(host="localhost", port=8888, virtualhost="foo"),
-            "amqp://guest:guest@localhost:8888/foo"
-        ),
-        (
-            dict(host="localhost", port=8888, virtualhost="/foo"),
-            "amqp://guest:guest@localhost:8888//foo"
-        ),
-    )
+class FakeConnection:
+    def __init__(self, url, **kwargs):
+        self.url = URL(url)
+        self.kwargs = kwargs
 
-    async def test_simple(self):
-        for kwargs, excpected in self.VARIANTS:
-            with self.subTest(excpected):
-                conn = await connect(
-                    connection_class=self.FakeConnection,
-                    **kwargs
-                )
+    async def connect(self):
+        return
 
-                self.assertEqual(conn.url, URL(excpected))
+
+@pytest.mark.parametrize("kwargs,expected", VARIANTS)
+def test_simple(kwargs, expected):
+    loop = asyncio.get_event_loop()
+    conn = loop.run_until_complete(connect(
+        connection_class=FakeConnection,
+        **kwargs
+    ))
+
+    assert conn.url == URL(expected)
