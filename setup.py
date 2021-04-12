e20286d0b8e70878728ba61f5d6ba55a638ac214
Dmitry Orlov <me@mosquito.su>
2021-04-12 16:32:00 +0300

    bump aiormq

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ],
     packages=find_packages(exclude=["tests"]),
     package_data={"aio_pika": ["py.typed"]},
-    install_requires=["aiormq~=4.2.0", "yarl"],
+    install_requires=["aiormq~=4.3.0", "yarl"],
     python_requires=">3.5.*, <4",
     extras_require={
         "develop": [
