b4c89d6c17d786a4f3e65fd513177ac189d3d2f6
Dmitry Orlov <me@mosquito.su>
2021-04-16 22:34:19 +0300

    fixes

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ setup(
     ],
     packages=find_packages(exclude=["tests"]),
     package_data={"aio_pika": ["py.typed"]},
-    install_requires=["aiormq~=5.2.0", "yarl"],    python_requires=">3.5.*, <4",
+    install_requires=["aiormq~=5.2.0", "yarl"],
+    python_requires=">3.5.*, <4",
     extras_require={
         "develop": [
             "aiomisc~=10.1.6",
