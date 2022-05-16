3f0d8e2b51201298bc2472dd89aafb207785af6c
Dmitry Orlov <me@mosquito.su>
2022-05-16 13:49:24 +0300

    version bump

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ],
     packages=find_packages(exclude=["tests"]),
     package_data={"aio_pika": ["py.typed"]},
-    install_requires=["aiormq~=6.2.3", "yarl"],
+    install_requires=["aiormq~=6.3.0", "yarl"],
     python_requires=">3.6, <4",
     extras_require={
         "develop": [
