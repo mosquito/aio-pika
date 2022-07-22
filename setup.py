3feec99c6615c06225844b60bdb8d6ac367600a6
deschmih <49398923+deschmih@users.noreply.github.com>
2022-07-22 07:00:57 +0200

    bump version to aiormq~=6.4.0  in setup.py
    because of comment https://github.com/mosquito/aio-pika/issues/481#issuecomment-1189961369 in issue

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     ],
     packages=find_packages(exclude=["tests"]),
     package_data={"aio_pika": ["py.typed"]},
-    install_requires=["aiormq~=6.3.2", "yarl"],
+    install_requires=["aiormq~=6.4.0", "yarl"],
     python_requires=">3.6, <4",
     extras_require={
         "develop": [
